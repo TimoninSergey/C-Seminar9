2b87125bd18291a1f07a088b8f12807dfaad6d4d
sergey_timonin <timonin-93@mail.ru>
2022-11-30 20:21:11 +0300

    Добавил задачи к девятому семинару

---
+++ Task64/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.
+
+N = 5 -> "4, 2"
+N = 8 -> "8, 6, 4, 2"
+*/
+
+Console.WriteLine("Введите N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+EvenNumbersNTo1(number);
+
+void EvenNumbersNTo1 (int n)
+{
+    if (n % 2 == 0 && n > 1)
+    {
+        Console.Write($"{n} ");
+        EvenNumbersNTo1(n-2);
+    }
+    else if (n % 2 == 1 && n > 1)
+    {
+        EvenNumbersNTo1(n-1);
+    }
+}
---
+++ Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.WriteLine("Введите меньшее число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите большее число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+Console.WriteLine($"Сумма чисел в промежутке от {number1} до {number2} равна {IntervalSumAToB(number1, number2, sum)}");
+
+
+int IntervalSumAToB (int a, int b, int sum)
+{
+    if (a <=b)
+    {
+        sum = sum + a;
+        a++;
+        return IntervalSumAToB(a, b, sum);
+    }
+    return sum;
+}
